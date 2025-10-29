e53c4175322ee3b08dd573e99a0abf95acfd2165
KelzangUD <kuding755@gmail.com>
2025-10-29 17:27:17 +0600

    seo init

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,52 @@ const inter = Inter({
   display: "swap",
 });
 
+const domainName = "https://jomolhari-vacation.vercel.app/"
+
 export const metadata: Metadata = {
-  title: "Jomolhari Vacation",
-  description: "Travel Agent Website",
+  metadataBase: new URL(`${domainName}`),
+  title: {
+    default: "Jomolhari Vacation",
+    template: "%s | Jomolhari Vacation",
+  },
+  description: "Experience the life of Highlanders in all Seasons with Reasons",
+  keywords: [
+    "Jomolhari Vacation",
+    "Bhutan Travel",
+    "Jomolhari",
+    "Vacation",
+    "Travel Agency",
+    "Bhutan tours",
+    "adventure travel",
+    "Bhutan Trekking",
+    "Bhutan Hiking",
+  ],
+  authors: [
+    {
+      name: "Jomolhari Vacation",
+      url: `${domainName}`,
+    },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_BT",
+    url: `${domainName}`,
+    title: "Jomolhari Vacation",
+    description:
+      "Experience the life of Highlanders in all Seasons with Reasons",
+    siteName: "Jomolhari Vacation",
+    images: [
+      {
+        url: "/logo-blue.svg",
+        width: 100,
+        height: 100,
+        alt: "Jomolhari Vacation",
+      },
+    ],
+  },
+  icons: {
+    icon: "/logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -19,9 +62,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-        </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }

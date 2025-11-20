96571533f3fc61fa2b5098d5724dbe71d3f7050e
KelzangUD <kuding755@gmail.com>
2025-11-20 11:45:46 +0600

    contact us page U

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,90 +1,18 @@
-"use client";
-
 import Image from "next/image";
-import { useState } from "react";
 
 export default function Contact() {
-  const [details, setDetails] = useState({
-    // name: "",
-    // emailId: "",
-    subject: "",
-    message: "",
-  });
   return (
     <div className="p-5 md:p-10 xl:p-15 2xl:p-20  bg-white">
       <h4>Contact Us</h4>
       <p className="my-2 xl:my-4">
         We’re here to help! Reach out to us with any questions or inquires about
-        our trekking/hiking adventure’s in Bhutan.
+        our trekking/hiking adventure’s in Bhutan. You can reach us directly via
+        phone or whatsapp or email.
       </p>
-      {/* <div className="mt-4 xl:mt-10">
-        <label className="text-xs md:text-base">Your Name</label>
-        <input
-          placeholder="Enter Your Name"
-          className="border border-border py-2 px-2 md:px-6 bg-border/10 rounded-md md:rounded-xl w-full my-3 text-xs md:text-base"
-        />
-      </div> */}
-      {/* <div className="xl:mt-2">
-        <label className="text-xs md:text-base">Your Email</label>
-        <input
-          placeholder="Enter Your Email"
-          className="border border-border py-2 px-2 md:px-6 bg-border/10 rounded-md md:rounded-xl w-full my-3 text-xs md:text-base"
-          onChange={(e) => setDetails((prev) => ({
-            ...prev,
-            email: e?.target?.value
-          }))}
-        />
-      </div> */}
-      <div className="xl:mt-2">
-        <label className="text-xs md:text-base">Subject</label>
-        <input
-          placeholder="Enter The Subject"
-          className="border border-border py-2 px-2 md:px-6 bg-border/10 rounded-md md:rounded-xl w-full my-3 text-xs md:text-base"
-          onChange={(e) =>
-            setDetails((prev) => ({
-              ...prev,
-              subject: e?.target?.value,
-            }))
-          }
-          value={details?.subject}
-        />
-      </div>
-      <div className="xl:mt-2">
-        <label className="text-xs md:text-base">Message</label>
-        <textarea
-          placeholder="Enter Your Message"
-          className="border border-border py-2 px-2 md:px-6 bg-border/10 rounded-md md:rounded-xl w-full my-3 text-xs md:text-base"
-          rows={6}
-          onChange={(e) =>
-            setDetails((prev) => ({
-              ...prev,
-              message: e?.target?.value,
-            }))
-          }
-          value={details?.message}
-        />
-      </div>
-      <div className="xl:mt-4">
-        <a
-          className={`bg-primary py-3 px-6 rounded-md overflow-hidden cursor-pointer text-sm lg:text-base text-white hover:bg-blue-700`}
-          href={`mailto:sonamvacation@gmail.com?subject=${details?.subject}&body=${details?.message}`}
-          onClick={() =>
-            setDetails((prev) => ({
-              ...prev,
-              subject: "",
-              message: "",
-            }))
-          }
-        >
-          Send Message
-        </a>
-      </div>
+      <p className="block bg-gray-100 rounded p-2 my-1 xl:my-2 italic text-xs md:text-sm">
+        (Please click on Icon button to contact us.)
+      </p>
       <div className="mt-6 xl:mt-10">
-        <p className="font-bold my-2">Direct Contact</p>
-        <p>
-          For immediate assistance, you can reach us directly via phone or
-          whatsapp
-        </p>
         <div className="flex items-center gap-2 xl:gap-10 my-4 xl:my-10">
           <a
             className="p-2 xl:p-4 rounded-md bg-blue-100 cursor-pointer"
@@ -103,6 +31,102 @@ export default function Contact() {
             <p className="text-sm xl:text-base">Phone/whatsapp info</p>
             <p className="text-secondary text-sm xl:text-base ">+97517914799</p>
           </span>
+        </div>
+        <div className="flex items-center gap-2 xl:gap-10 my-4 xl:my-10">
+          <a
+            className="p-2 xl:p-4 rounded-md bg-blue-100 cursor-pointer"
+            href="mailto:sonamvacation@gmail.com"
+          >
+            <div className="relative w-4 h-4 2xl:w-6 2xl:h-6">
+              <Image
+                src="/Mail.svg"
+                alt="phone"
+                fill
+                className="object-cover"
+              />
+            </div>
+          </a>
+          <span className="ml-2">
+            <p className="text-sm xl:text-base">Mail</p>
+            <p className="text-secondary text-sm xl:text-base ">
+              sonamvacation@gmail.com
+            </p>
+          </span>
+        </div>
+      </div>
+      <div>
+        <h4>Social Network</h4>
+        <div className="flex gap-5 mt-6 md:mt-8">
+          <a
+            href="https://wa.me/+97517914799"
+            target="_blank"
+            className="cursor-pointer transition duration-300 ease-in-out hover:scale-115 bg-gray-200 rounded"
+          >
+            <div className="relative w-6 h-6 md:w-10 md:h-10">
+              <Image
+                src="/whatsapp.svg"
+                alt="whatsapp"
+                fill
+                className="object-cover p-2"
+              />
+            </div>
+          </a>
+          <a
+            href="https://www.facebook.com/jomolharivacationtour/"
+            target="_blank"
+            className="cursor-pointer transition duration-300 ease-in-out hover:scale-115 bg-gray-200 rounded"
+          >
+            <div className="relative w-6 h-6 md:w-10 md:h-10">
+              <Image
+                src="/facebook.svg"
+                alt="facebook"
+                fill
+                className="object-cover p-2"
+              />
+            </div>
+          </a>
+          <a
+            href="https://www.instagram.com/jomolhari_vacation/"
+            target="_blank"
+            className="cursor-pointer transition duration-300 ease-in-out hover:scale-115 bg-gray-200 rounded"
+          >
+            <div className="relative w-6 h-6 md:w-10 md:h-10">
+              <Image
+                src="/instagram.svg"
+                alt="instagram"
+                fill
+                className="object-cover p-2"
+              />
+            </div>
+          </a>
+          <a
+            href="https://www.tiktok.com/@jomolharivac"
+            target="_blank"
+            className="cursor-pointer transition duration-300 ease-in-out hover:scale-115 bg-gray-200 rounded"
+          >
+            <div className="relative w-6 h-6 md:w-10 md:h-10">
+              <Image
+                src="/tik-tok.svg"
+                alt="whatsapp"
+                fill
+                className="object-cover p-2"
+              />
+            </div>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sonam-rinchen-44571816a/"
+            target="_blank"
+            className="cursor-pointer transition duration-300 ease-in-out hover:scale-115 bg-gray-200 rounded"
+          >
+            <div className="relative w-6 h-6 md:w-10 md:h-10">
+              <Image
+                src="/linkedin.svg"
+                alt="linkedin"
+                fill
+                className="object-cover p-2"
+              />
+            </div>
+          </a>
         </div>
       </div>
     </div>
